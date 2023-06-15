525c9d3c2f92af57250713e50be1d07586611f95
Sierra <sierra.butler@bryanuniversity.edu>
2023-06-14 17:36:32 -0700

    second

---
+++ week3/user.js
@@ -0,0 +1,11 @@
+const user = require("./executingAProgram")
+
+
+console.log( "The sum of ${num1} and ${num2} is ${sum}");
+console.log( "The difference of ${num1} and ${num2} is ${sum}");
+console.log( "The product of ${num1} and ${num2} is ${sum}");
+console.log( "The quotient of ${num1} and ${num2} is ${sum}");
+
+console.log(greeting, "Hello, ${userName} who is ${userAge}, welcome!");
+
+console.log(numbers)
