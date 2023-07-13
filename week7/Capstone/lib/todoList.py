809a0d17fa67a318a2b1132da1e01bf0ea14a726
Sierra <sierra.butler@bryanuniversity.edu>
2023-07-12 19:19:36 -0700

    saved1

--- week7/Capstone/lib/todoList.py
+++ week7/Capstone/lib/todoList.py
@@ -4,18 +4,22 @@ class todoList:
     
     def addList(self, item):
         self.list.append(item)
-
+        return self.list
+    
     def addMult(self, items):
         self.list.extend(items)
-
+        return self.list
+    
     def removedFirst(self):
-        self.list.remove()
-
+        self.list.pop(0)
+        return self.list
+    
     def removedLast(self):
-        self.list.pop(len(self.list -1))
-
+        self.list.pop()
+        return self.list
+    
     def specific(self, item):
         index = self.list.index(item)
         if index:
             self.list.pop(index)
-
+        return self.list
--- week7/Capstone/test/test_todoList.py
+++ week7/Capstone/test/test_todoList.py
@@ -1,8 +1,33 @@
 import pytest
-
+from lib.todoList import todoList
 
 @pytest.fixture 
-def testaddList():
+def xList():
     x = todoList()
-    assert x.addList(23) == [23]
+    
+    return x
 
+def testAddList(xList):
+    # x = todoList()
+    assert xList.addList(23) == [23]
+
+def testAddMult(xList):
+    # x = todoList()
+    assert xList.addMult(["blue", "green", "yellow"]) == ["blue", "green", "yellow"]
+
+def testRemovedFirst(xList):
+    # x = todoList()
+    xList.addList("rainbow")
+    xList.addList("unicorn")
+    assert xList.removedFirst() == ["unicorn"]
+
+def testRemovedLast(xList):
+    # x = todoList()
+    xList.addList("rainbow")
+    xList.addList("unicorn")
+    assert xList.removedLast() == ["rainbow"]
+
+def testSpecific(xList):
+    # x = todoList
+    xList.addMult(["blue", "green", "yellow"])
+    assert xList.specific("green") == ["blue", "yellow"]
